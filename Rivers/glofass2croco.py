aef9cda27b88a886987784ecadf051b43739ca56
Adi Sujiwo <sujiwo@gmail.com>
2025-06-02 07:28:44 +0700

    Migrate to OOP

--- Rivers/glofass2croco.py
+++ Rivers/glofass2croco.py
@@ -3,6 +3,41 @@ import numpy as np
 import rasterio as rio
 import rasterio.plot
 from copy import copy
+import warnings
+
+
+class Glofass:
+    def __init__(self, glPath):
+        self.fd = nc4.Dataset(glPath)
+        # Filter all valid sources
+        discharges = self.fd['dis24'][0,:,:]
+        coordshapes = discharges.shape
+        self.truerivers = []
+        self.valid_times = np.array(self.fd['valid_time'][:], dtype=self.fd['valid_time'].dtype)
+        self.latitude = np.array(self.fd['latitude'])
+        self.longitude = np.array(self.fd['longitude'])
+        for lat_n in range(coordshapes[0]):
+            for lon_n in range(coordshapes[1]):
+                if discharges.mask[lat_n, lon_n]==True:
+                    continue
+                latitude = float(self.fd['latitude'][lat_n])
+                longitude = float(self.fd['longitude'][lon_n])
+                self.truerivers.append({'lat':latitude, 
+                    'lon':longitude, 
+                    'index': (lat_n,lon_n)}
+                )
+
+    def filter_rivers(self, lat_min, lat_max, lon_min, lon_max):
+        self.truerivers = [R for R in self.truerivers 
+                           if R['lat']>=lat_min and R['lat']<=lat_max
+                           and R['lon']>=lon_min and R['lon']<=lon_max
+                          ]
+        self.latitude = self.latitude[
+            (self.latitude>=lat_min) & (self.latitude<=lat_max)
+            ]
+        self.longitude = self.longitude[
+            (self.longitude>=lon_min) & (self.longitude<=lon_max)
+            ]
 
 
 class CGrid:
@@ -58,19 +93,53 @@ class CGrid:
     def where(self, lat, lon, kind='rho'):
         lat_tg, lon_tg, _ = self._select_table(kind)
         xd = np.searchsorted(lon_tg, lon)
-        yd = np.searchsorted(lat_tg, lat)
+        yd = np.searchsorted(lat_tg, lat, side='right')
         return (xd,yd)
     
     def query(self, lat, lon, kind='rho'):
         lat_tg, lon_tg, table = self._select_table(kind)
         xd = np.searchsorted(lon_tg, lon)
-        yd = np.searchsorted(lat_tg, lat)
+        yd = np.searchsorted(lat_tg, lat, side='right')
         return table[yd, xd]
 
-# glosrcname = 'DATA/glofas_merged_NATUNA.nc'
-# crocdstname = 'CROCO_FILES/croco_runoff_test.nc'
-# gridname = 'CROCO_FILES/croco_grd.nc'
+def convert_glofass2croco(glosrc_, crocogrd_):
+    glofd = nc4.Dataset(glosrc_)
+    cgrid = CGrid(crocogrd_)
+    
+    if glofd['latitude'][0] < cgrid.latitudes_r[0] or \
+        glofd['latitude'][-1] > cgrid.latitudes_r[-1]:
+            warnings.warn("Glofass latitude is outside grid range")
+    if glofd['longitude'][0] < cgrid.longitudes_r[0] or \
+        glofd['longitude'][-1] > cgrid.longitudes_r[-1]:
+            warnings.warn("Glofass longitude is outside grid range")
 
+    discharges = glofd['dis24'][0,:,:]
+    coordshapes = discharges.shape
+    truerivers = []
+    for lat_n in range(coordshapes[0]):
+        for lon_n in range(coordshapes[1]):
+            if discharges.mask[lat_n, lon_n]==True:
+                continue
+            latitude = float(glofd['latitude'][lat_n])
+            longitude = float(glofd['longitude'][lon_n])
+            # Find position in domain grid
+            lon_rg, lat_rg = cgrid.where(latitude, longitude)
+            # if cgrid.mask_r[lat_rg, lon_rg]==False:
+            pass
+
+
+if __name__=='__main__':
+    glosrcname = '/home/sujiwo/Data/Natuna/DATA/glofas_merged_NATUNA.nc'
+    crocdstname = '/home/sujiwo/Data/Natuna/CROCO_FILES/croco_runoff_test.nc'
+    gridname = '/home/sujiwo/Data/Natuna/CROCO_FILES/croco_grd.nc'
+
+    cgrid = CGrid(gridname)
+    glo = Glofass((glosrcname))
+    glo.filter_rivers(cgrid.latitudes_r[0], cgrid.latitudes_r[-1], 
+                      cgrid.longitudes_r[0], cgrid.longitudes_r[-1])
+
+    # convert_glofass2croco(glosrcname, gridname)
+    pass
 # glosrc = nc4.Dataset(glosrcname)
 # grid = nc4.Dataset(gridname)
 
